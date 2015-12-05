ea58eb8d94f81e93e63b057e881748312f0d1161
opengl8080 <tomcat.port.8080+github@gmail.com>
2015-12-05 18:25:27 +0900

    Javadoc 記述

--- src/main/java/gl8080/lifegame/logic/CellDefinition.java
+++ src/main/java/gl8080/lifegame/logic/CellDefinition.java
@@ -1,13 +1,24 @@
 package gl8080.lifegame.logic;
 
+/**
+ * セル定義を表すクラス。
+ */
 public class CellDefinition {
 
     private boolean alive;
 
+    /**
+     * このセル定義の生死の状態を設定する。
+     * @param alive 生存の場合は {@code true} を指定する。
+     */
     public void setStatus(boolean alive) {
         this.alive = alive;
     }
     
+    /**
+     * このセル定義が生きているかどうかを確認する。
+     * @return 生きている場合は {@code true}
+     */
     public boolean isAlive() {
         return this.alive;
     }
