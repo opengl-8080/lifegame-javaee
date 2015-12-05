d49b0e8aa493264851842dc13f1954b27da39aca
opengl8080 <tomcat.port.8080+github@gmail.com>
2015-12-05 18:42:25 +0900

    Javadoc 記述

--- src/main/java/gl8080/lifegame/logic/Game.java
+++ src/main/java/gl8080/lifegame/logic/Game.java
@@ -5,17 +5,33 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+/**
+ * ゲームを表すクラス。
+ */
 public class Game {
     
     private Map<Position, Cell> cells;
 
+    /**
+     * 新しいゲームを作成する。
+     * 
+     * @param gameDef このゲームの元となるゲーム定義
+     * @throws NullPointerException ゲーム定義が {@code null} の場合
+     */
     public Game(GameDefinition gameDef) {
         this.cells = new HashMap<>();
         
+        this.initializeCells(gameDef);
+        this.setupNeighborCells();
+    }
+
+    private void initializeCells(GameDefinition gameDef) {
         gameDef.getCellDefinitions().forEach((position, cellDef) -> {
             this.cells.put(position, cellDef.isAlive() ? Cell.alive() : Cell.dead());
         });
-        
+    }
+
+    private void setupNeighborCells() {
         this.cells.forEach((position, cell) -> {
             List<Cell> neighbors =
                 position
@@ -28,13 +44,23 @@ public class Game {
         });
     }
 
+    /**
+     * このゲームが持つ全てのセルを取得する。
+     * <p>
+     * このメソッドが返すマップは、このオブジェクトが持つマップのコピーです。<br>
+     * ここで取得したマップのエントリを削除するなどしても、このオブジェクトが持つオリジナルのマップには影響を与えません。
+     * 
+     * @return このゲームが持つ全てのセル
+     */
     public Map<Position, Cell> getCells() {
         return new HashMap<>(this.cells);
     }
 
+    /**
+     * このゲームを１ステップ進める。
+     */
     public void nextStep() {
         this.cells.values().forEach(Cell::reserveNextStatus);
         this.cells.values().forEach(Cell::stepNextStatus);
     }
-
 }
