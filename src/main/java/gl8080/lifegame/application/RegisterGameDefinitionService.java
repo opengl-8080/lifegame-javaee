a68c97a337e1a01b14f4dfc61203a4260c4340ac
opengl8080 <tomcat.port.8080+github@gmail.com>
2015-12-10 23:45:04 +0900

    登録サービス追加

---
+++ src/main/java/gl8080/lifegame/application/RegisterGameDefinitionService.java
@@ -0,0 +1,25 @@
+package gl8080.lifegame.application;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+
+import gl8080.lifegame.logic.GameDefinition;
+import gl8080.lifegame.logic.GameDefinitionRepository;
+
+@Stateless
+public class RegisterGameDefinitionService {
+    @Inject
+    private Logger logger;
+    
+    @Inject
+    private GameDefinitionRepository gameDefinitionRepository;
+    
+    public GameDefinition register(int size) {
+        logger.info("register game definition (size={})", size);
+        GameDefinition gameDefinition = new GameDefinition(size);
+        this.gameDefinitionRepository.register(gameDefinition);
+        return gameDefinition;
+    }
+}
