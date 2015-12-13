4a9e2201bb9a6e677367dc7fdc4e48010fe4dc9f
opengl8080 <tomcat.port.8080+github@gmail.com>
2015-12-14 00:46:31 +0900

    ビジネスエラーの Mapper 追加

---
+++ src/main/java/gl8080/lifegame/persistence/BusinessExceptionMapper.java
@@ -0,0 +1,17 @@
+package gl8080.lifegame.persistence;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import gl8080.lifegame.logic.exception.BusinessException;
+
+@Provider
+public class BusinessExceptionMapper implements ExceptionMapper<BusinessException> {
+
+    @Override
+    public Response toResponse(BusinessException e) {
+        return Response.status(Status.BAD_REQUEST).build();
+    }
+}
