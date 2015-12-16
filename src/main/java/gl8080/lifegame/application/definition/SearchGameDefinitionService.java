1843a1d4ac2b6d25b9d27e8d7bc45a658dde5845
opengl8080 <tomcat.port.8080+github@gmail.com>
2015-12-16 14:05:21 +0900

    application -> web への依存を最小限に

--- src/main/java/gl8080/lifegame/application/definition/SearchGameDefinitionService.java
+++ src/main/java/gl8080/lifegame/application/definition/SearchGameDefinitionService.java
@@ -7,7 +7,6 @@ import org.slf4j.Logger;
 
 import gl8080.lifegame.logic.definition.GameDefinition;
 import gl8080.lifegame.logic.definition.GameDefinitionRepository;
-import gl8080.lifegame.web.resource.LifeGameDto;
 
 @Stateless
 public class SearchGameDefinitionService {
@@ -16,11 +15,11 @@ public class SearchGameDefinitionService {
     @Inject
     private GameDefinitionRepository repository;
     
-    public LifeGameDto search(long id) {
+    public GameDefinition search(long id) {
         logger.debug("search game definition (id={})", id);
         
         GameDefinition gameDefinition = this.repository.search(id);
         
-        return LifeGameDto.of(gameDefinition);
+        return gameDefinition;
     }
 }
